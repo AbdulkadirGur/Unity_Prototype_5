33e2afe4325e86377770ed1de4edbc0954e215d1
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2024-03-20 12:30:10 +0300

    .

--- Unity_Prototype_5/Assets/Scripts/ClickAndSwipe.cs
+++ Unity_Prototype_5/Assets/Scripts/ClickAndSwipe.cs
@@ -8,7 +8,8 @@ public class ClickAndSwipe : MonoBehaviour
 {
     private GameManager gameManager;
     private Camera cam;
-    private Vector3 mousePos;
+    //private Vector3 mousePos;
+    private Vector3 touchPos;
 
     private TrailRenderer trail;
     private BoxCollider col;
@@ -27,6 +28,7 @@ public class ClickAndSwipe : MonoBehaviour
    
     void Update()
     {
+        /*
         if (gameManager.isGameActive)
         {
             if (Input.GetMouseButtonDown(0))
@@ -43,23 +45,73 @@ public class ClickAndSwipe : MonoBehaviour
             {
                 UpdateMousePosition();
             }
+        }*/
+
+        if (gameManager.isGameActive)
+        {
+            // Dokunmatik ekran giri?lerini kontrol et
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                // Dokunma ba?lang?c?
+                if (touch.phase == TouchPhase.Began)
+                {
+                    swiping = true;
+                    UpdateComponents();
+                }
+                // Dokunma sonu
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    swiping = false;
+                    UpdateComponents();
+                }
+
+                // Dokunma devam ederken
+                if (swiping)
+                {
+                    UpdateTouchPosition(touch);
+                }
+            }
         }
 
     }
-
+    /*
     void UpdateMousePosition()
     {
         mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
         Input.mousePosition.y, 10.0f));
         transform.position = mousePos;
+    }*/
+    // Dokunma pozisyonunu güncelle
+    void UpdateTouchPosition(Touch touch)
+    {
+        touchPos = cam.ScreenToWorldPoint(new Vector3(touch.position.x,
+            touch.position.y, 10.0f));
+        transform.position = touchPos;
     }
-
+    /*
+    void UpdateComponents()
+    {
+        trail.enabled = swiping;
+        col.enabled = swiping;
+    }*/
+    // Iz sürücü ve collider'? güncelle
     void UpdateComponents()
     {
         trail.enabled = swiping;
         col.enabled = swiping;
     }
 
+    /*
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Target>() )
+        {
+            collision.gameObject.GetComponent<Target>().DestroyTarget();
+        }
+    }*/
+    // Çarp??ma olay?n? kontrol et
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Target>())
